eceeac31226657591ed3993b187e59af2f27e6f7
loganmetzger <logan.c.metzger@gmail.com>
2021-05-30 21:01:43 -0400

    Updated product list ui

--- Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
@@ -1,23 +1,30 @@
 package com.imobile3.groovypayments.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.imobile3.groovypayments.R;
+import com.imobile3.groovypayments.data.enums.GroovyColor;
+import com.imobile3.groovypayments.data.enums.GroovyIcon;
 import com.imobile3.groovypayments.data.model.Product;
+import com.imobile3.groovypayments.rules.CurrencyRules;
 import com.imobile3.groovypayments.utils.StateListHelper;
 
 import org.jetbrains.annotations.NotNull;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ProductListAdapter
         extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
@@ -52,7 +59,12 @@ public class ProductListAdapter
     public void onBindViewHolder(ViewHolder holder, int position) {
         Product item = mItems.get(position);
 
+        holder.icon.setBackgroundColor(this.mContext.getResources().getColor(GroovyColor.fromId(item.getColorId()).colorRes));
+        holder.icon.setImageDrawable(this.mContext.getDrawable(GroovyIcon.fromId(item.getIconId()).drawableRes));
         holder.label.setText(item.getName());
+        holder.priceAndNote.setText(String.format(this.mContext.getString(R.string.product_item_price_description),
+                new CurrencyRules().getFormattedAmount(item.getUnitPrice(), Locale.getDefault()),
+                item.getNote()));
         holder.label.setTextColor(
                 StateListHelper.getTextColorSelector(mContext, R.color.black_space));
     }
@@ -64,12 +76,16 @@ public class ProductListAdapter
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ViewGroup container;
+        ImageView icon;
         TextView label;
+        TextView priceAndNote;
 
         ViewHolder(View itemView) {
             super(itemView);
             container = itemView.findViewById(R.id.container);
+            icon = itemView.findViewById(R.id.icon);
             label = itemView.findViewById(R.id.label);
+            priceAndNote = itemView.findViewById(R.id.unit_price_and_note);
             container.setOnClickListener(this);
         }
 
